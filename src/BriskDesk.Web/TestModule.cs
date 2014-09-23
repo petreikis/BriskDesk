696d7163cb37c6b08317a2dc2f153055b8ffe365
Karolis <karolis.petreikis@gmail.com>
2014-09-23 23:11:35 +0300

    #4 - Set up NancyFX project

---
+++ src/BriskDesk.Web/TestModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace BriskDesk.Web
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = _ =>
+            {
+                return "This is a test module";
+            };
+        }
+    }
+}
